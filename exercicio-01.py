b89d162903436c375a8b51b8ee225842f1781ec2
Joyce Silva <joycejsm96@protonmail.com>
2025-03-10 19:54:41 -0300

    add: exercicio 01 fix: mudei nome do arquivo, aula 04 para aula-02 add: adicionei exericicio 03

---
+++ exercicio-01.py
@@ -0,0 +1,8 @@
+fruta = "melão"
+print(fruta)
+
+#Expressões matemáticas
+print(2*3+3*2)
+
+carro = "fox"
+print(carro)
---
+++ exercicio-03.py
@@ -0,0 +1,18 @@
+#Desafio
+
+#Criar um programa para verificar a MaiorIdade
+
+#Passo 1: Criar uma variavel 
+#Passo 2: Criar uma estrutura de condição 
+#Passo: Exibir a frase adequada de acordo com a idade da pessoa.
+
+idade = int(input("Digite sua idade"))
+
+if idade  >= 18:
+    print("Você é maior de idade")
+
+elif idade >=12:
+    print("Você é um adolescente")
+
+else:
+    print("Você uma criança")
